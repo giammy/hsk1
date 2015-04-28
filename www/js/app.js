126fe0732f2a373d55d7b880eb2f078f442fec15
giammy <giangiammy@gmail.com>
2015-04-28 15:39:40 +0200

    go back to onsen old version - new one has a slow refresh

--- www/js/app.js
+++ www/js/app.js
@@ -11,16 +11,29 @@
   module.controller('DetailController', function($scope, $data) {
     $scope.item = $data.selectedItem;
 
+    $scope.backReset = function(index) {
+	var pages = $scope.navi.getPages();
+	var i = 1;
+	/* console.log("pages.length=%i", pages.length); */
+	while (pages.length > 2) {
+ 	  pages[pages.length - 1].destroy();
+	}
+	$scope.navi.popPage();
+    };
+
     $scope.showReplaceDetail = function(index) {
       var selectedItem = $data.items[index];
       $data.selectedItem = selectedItem;
-      $scope.navi.replacePage('detail.html', {title : selectedItem.title});
+      /*      $scope.navi.replacePage('detail.html', {title : selectedItem.title}); */
+      /*      $scope.navi.popPage(); */
+      $scope.navi.pushPage('detail.html', {title : selectedItem.title});
     };
 
     $scope.showReplaceRandomDetail = function() {
       var selectedItem = $data.items[Math.floor(Math.random() * 153)];
       $data.selectedItem = selectedItem;
-      $scope.navi.replacePage('detail.html', {title : selectedItem.title});
+      /* $scope.navi.replacePage('detail.html', {title : selectedItem.title}); */
+      $scope.navi.pushPage('detail.html', {title : selectedItem.title});
     };
 
   });
